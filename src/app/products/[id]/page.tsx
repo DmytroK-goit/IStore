5c30018762cc91490a3a7d234fc119c087249a0e
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2025-08-18 22:15:12 +0300

    add any

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,12 +1,4 @@
-interface ProductPageParams {
-  id: string;
-}
-
-export default async function ProductPage({
-  params,
-}: {
-  params: ProductPageParams;
-}) {
+export default function ProductPage({ params }: any) {
   return (
     <div>
       <h1>Product details</h1>
