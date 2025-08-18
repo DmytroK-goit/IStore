e51628e6dd9cfd21f2960ed3fd78972faf9dcbb1
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2025-08-18 22:11:11 +0300

    add interface

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,10 +1,16 @@
-export default async function ProductPage({ params }: any) {
-  const { id } = params;
+interface ProductPageParams {
+  id: string;
+}
 
+export default async function ProductPage({
+  params,
+}: {
+  params: ProductPageParams;
+}) {
   return (
     <div>
       <h1>Product details</h1>
-      <p>Product ID: {id}</p>
+      <p>Product ID: {params.id}</p>
     </div>
   );
 }
