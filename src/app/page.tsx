744b923e2b600fa5d16b017becf70c205160da82
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2025-08-19 22:20:47 +0300

    add style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,86 @@ export default function Home() {
     <div>
       <h1>Welcome to IStore</h1>
       <p>Your one-stop shop for all things tech!</p>
-      <div className="flex gap-4 mt-4">
-        <Link href="/about">About Us</Link>
-        <Link href="/products">Products</Link>
-        <Link href="/login">Login</Link>
-        <Link href="/register">Registration</Link>
+      <div
+        className="flex gap-4 
+      mt-4"
+      >
+        <Link
+          href="/about"
+          className="    inline-block    px-6 py-2    text-white font-semibold
+    rounded-2xl
+    bg-emerald-500
+    shadow-md
+    hover:bg-emerald-600
+    hover:shadow-lg
+    transition
+    duration-300
+    ease-in-out
+    text-center
+  "
+        >
+          About Us
+        </Link>
+        <Link
+          className="
+    inline-block
+    px-6 py-2
+    text-white
+    font-semibold
+    rounded-2xl
+    bg-emerald-500
+    shadow-md
+    hover:bg-emerald-600
+    hover:shadow-lg
+    transition
+    duration-300
+    ease-in-out
+    text-center
+  "
+          href="/products"
+        >
+          Products
+        </Link>
+        <Link
+          className="
+    inline-block
+    px-6 py-2
+    text-white
+    font-semibold
+    rounded-2xl
+    bg-emerald-500
+    shadow-md
+    hover:bg-emerald-600
+    hover:shadow-lg
+    transition
+    duration-300
+    ease-in-out
+    text-center
+  "
+          href="/login"
+        >
+          Login
+        </Link>
+        <Link
+          className="
+    inline-block
+    px-6 py-2
+    text-white
+    font-semibold
+    rounded-2xl
+    bg-emerald-500
+    shadow-md
+    hover:bg-emerald-600
+    hover:shadow-lg
+    transition
+    duration-300
+    ease-in-out
+    text-center
+  "
+          href="/register"
+        >
+          Registration
+        </Link>
       </div>
     </div>
   );
