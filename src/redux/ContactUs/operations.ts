68dd94ee05779b41d3e9675485501052da57b0ec
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2025-09-03 16:21:11 +0300

    add redux to ContactUs

---
+++ src/redux/ContactUs/operations.ts
@@ -0,0 +1,4 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { istore } from '../UserAuth/operations';
+import { Product } from '@/types/product';
+import { toast } from 'react-toastify';
