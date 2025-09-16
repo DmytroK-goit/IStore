60491feb06c0064652848426439878633c65ee1b
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2025-09-16 11:36:53 +0300

    Add Cloudinary to allowed image domains
    Included 'res.cloudinary.com' in the Next.js image domains configuration to support loading images from Cloudinary.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['i-store-lyart.vercel.app'],
+    domains: ['i-store-lyart.vercel.app', 'res.cloudinary.com'],
   },
 
   compress: true,
