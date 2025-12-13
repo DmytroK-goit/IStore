b677ce99df8e47f19106b7a074ea4384bcf28ba4
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2025-12-13 16:12:01 +0200

    add gif animayion in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,7 +12,15 @@ import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { selectIsLoading } from '@/redux/UserAuth/selectors';
 import { GridLoader } from 'react-spinners';
+import { useState } from 'react';
 
+type GifState = 'wait' | 'success' | 'error';
+
+const gifs = {
+  wait: '/gifts/load.gif',
+  success: '/gifts/done.gif',
+  error: '/gifts/wrong.gif',
+};
 interface LoginFormValues {
   email: string;
   password: string;
@@ -23,6 +31,7 @@ export default function Login() {
   const loading = useSelector(selectIsLoading);
   const dispatch = useDispatch<AppDispatch>();
   const initialValues: LoginFormValues = { email: '', password: '' };
+  const [gifState, setGifState] = useState<GifState>('wait');
 
   const validationSchema = Yup.object({
     email: Yup.string().email('Invalid email address').required('Email is required'),
@@ -36,29 +45,33 @@ export default function Login() {
     { setSubmitting }: FormikHelpers<LoginFormValues>,
   ) => {
     try {
+      setGifState('wait');
+
       const resultAction = await dispatch(login(values));
+
       if (login.fulfilled.match(resultAction)) {
+        setGifState('success');
+
         const userRole = resultAction.payload.data.user.role;
-        router.push(userRole === 'admin' ? '/admin' : '/products');
+
+        setTimeout(() => {
+          router.push(userRole === 'admin' ? '/admin' : '/products');
+        }, 800);
       } else {
+        setGifState('error');
         toast.error('Login failed. Please check your credentials.');
       }
     } catch {
+      setGifState('error');
       toast.error('An unexpected error occurred. Please try again.');
     } finally {
       setSubmitting(false);
     }
   };
+
   const handleRegisterRedirect = () => {
     router.push('/register');
   };
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <GridLoader color="#22c55e" />
-      </div>
-    );
-  }
 
   return (
     <main className="relative min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat">
@@ -72,75 +85,83 @@ export default function Login() {
         initial={{ opacity: 0, y: 30 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
-        className="backdrop-blur-sm bg-black/60 p-8 rounded-2xl shadow-lg max-w-md w-full mx-4"
+        className="backdrop-blur-sm bg-black/60 p-8 rounded-2xl shadow-lg max-w-xl w-full mx-4"
       >
         <h2 className="text-3xl font-bold mb-6 text-yellow-400 text-center">Login to IStore</h2>
+        <div className="flex gap-4">
+          <div>
+            <img src={gifs[gifState]} alt="status animation" className="w-40 h-40 object-contain" />
+          </div>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ isSubmitting }) => (
+              <Form className="flex flex-col gap-5">
+                <div>
+                  <label htmlFor="email" className="block text-sm font-semibold text-gray-200 mb-1">
+                    Email
+                  </label>
+                  <Field
+                    type="email"
+                    name="email"
+                    className="w-full p-3 rounded-lg bg-gray-800 text-gray-100 border border-gray-700 focus:ring-2 focus:ring-emerald-500 outline-none"
+                    placeholder="Enter your email"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="text-red-400 text-sm mt-1"
+                  />
+                </div>
 
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={handleSubmit}
-        >
-          {({ isSubmitting }) => (
-            <Form className="flex flex-col gap-5">
-              <div>
-                <label htmlFor="email" className="block text-sm font-semibold text-gray-200 mb-1">
-                  Email
-                </label>
-                <Field
-                  type="email"
-                  name="email"
-                  className="w-full p-3 rounded-lg bg-gray-800 text-gray-100 border border-gray-700 focus:ring-2 focus:ring-emerald-500 outline-none"
-                  placeholder="Enter your email"
-                />
-                <ErrorMessage name="email" component="div" className="text-red-400 text-sm mt-1" />
-              </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-semibold text-gray-200 mb-1"
+                  >
+                    Password
+                  </label>
+                  <Field
+                    type="password"
+                    name="password"
+                    className="w-full p-3 rounded-lg bg-gray-800 text-gray-100 border border-gray-700 focus:ring-2 focus:ring-emerald-500 outline-none"
+                    placeholder="Enter your password"
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="text-red-400 text-sm mt-1"
+                  />
+                </div>
 
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-semibold text-gray-200 mb-1"
+                <motion.button
+                  initial={{ x: 5 }}
+                  animate={{ x: 0 }}
+                  whileHover={{ scale: 1.02 }}
+                  whileTap={{ scale: 0.9 }}
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="bg-emerald-600 hover:bg-emerald-500 transition text-white font-semibold py-3 rounded-lg shadow-md disabled:opacity-50"
                 >
-                  Password
-                </label>
-                <Field
-                  type="password"
-                  name="password"
-                  className="w-full p-3 rounded-lg bg-gray-800 text-gray-100 border border-gray-700 focus:ring-2 focus:ring-emerald-500 outline-none"
-                  placeholder="Enter your password"
-                />
-                <ErrorMessage
-                  name="password"
-                  component="div"
-                  className="text-red-400 text-sm mt-1"
-                />
-              </div>
-
-              <motion.button
-                initial={{ x: 5 }}
-                animate={{ x: 0 }}
-                whileHover={{ scale: 1.02 }}
-                whileTap={{ scale: 0.9 }}
-                type="submit"
-                disabled={isSubmitting}
-                className="bg-emerald-600 hover:bg-emerald-500 transition text-white font-semibold py-3 rounded-lg shadow-md disabled:opacity-50"
-              >
-                {isSubmitting ? 'Logging in...' : 'Login'}
-              </motion.button>
-              <motion.button
-                onClick={handleRegisterRedirect}
-                initial={{ x: -5 }}
-                animate={{ x: 0 }}
-                whileHover={{ scale: 1.02 }}
-                whileTap={{ scale: 0.9 }}
-                type="button"
-                className="bg-transparent border-2 border-emerald-600 text-emerald-600 font-semibold py-3 rounded-lg shadow-md hover:bg-emerald-600 hover:text-white transition disabled:opacity-50 cursor-pointer"
-              >
-                Register
-              </motion.button>
-            </Form>
-          )}
-        </Formik>
+                  {isSubmitting ? 'Logging in...' : 'Login'}
+                </motion.button>
+                <motion.button
+                  onClick={handleRegisterRedirect}
+                  initial={{ x: -5 }}
+                  animate={{ x: 0 }}
+                  whileHover={{ scale: 1.02 }}
+                  whileTap={{ scale: 0.9 }}
+                  type="button"
+                  className="bg-transparent border-2 border-emerald-600 text-emerald-600 font-semibold py-3 rounded-lg shadow-md hover:bg-emerald-600 hover:text-white transition disabled:opacity-50 cursor-pointer"
+                >
+                  Register
+                </motion.button>
+              </Form>
+            )}
+          </Formik>
+        </div>
       </motion.div>
     </main>
   );
